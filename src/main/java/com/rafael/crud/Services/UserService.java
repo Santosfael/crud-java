db490c96e269f9e6722e765c2259b5f1b62d3148
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-04-21 10:50:15 -0300

    feat: implementing router delete user by id

--- src/main/java/com/rafael/crud/Services/UserService.java
+++ src/main/java/com/rafael/crud/Services/UserService.java
@@ -40,4 +40,13 @@ public class UserService {
     public List<User> listUsers() {
         return userRepository.findAll();
     }
+
+    public void deleteUserById(String userId) {
+        var id = UUID.fromString(userId);
+        var userExists = userRepository.existsById(id);
+
+        if (userExists) {
+            userRepository.deleteById(id);
+        }
+    }
 }
